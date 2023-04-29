1ffa0c875b98ed6e1efda1766b8f5968e529baaa
Kd-Here <gamelover9899@gmail.com>
2023-04-29 19:31:09 +0530

    Setup file created

---
+++ End-To-End-ML/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='mlpackage',
+    version='1.0.0',
+    author='Kd-Here',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
