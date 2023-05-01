1b4578fd94ee59294a0ad454cc930f87b47eaef7
Kd-Here <gamelover9899@gmail.com>
2023-05-01 10:32:16 +0530

    Updated logger and exception file

--- End-To-End-ML/src/exception.py
+++ End-To-End-ML/src/exception.py
@@ -13,7 +13,6 @@ class CustomException(Exception):
     
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self) -> str:
@@ -25,5 +24,5 @@ if __name__ == "__main__":
     try:
         a = 1/0
     except Exception as e:
-        logging.info('Divide by Zero !')
         raise CustomException(e,sys)
+        logging.info('Divide by Zero !')
